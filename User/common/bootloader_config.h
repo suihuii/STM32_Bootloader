3ad2814ddd8ced7199f7a4b26ee86475063bf10e
suihuii <suihui1034574609@gmail.com>
2026-08-06 21:38:29 +0800

    feat: add V3 bootloader project skeleton

---
+++ User/common/bootloader_config.h
@@ -0,0 +1,6 @@
+#ifndef __BOOTLOADER_CONFIG_H
+#define __BOOTLOADER_CONFIG_H
+
+
+
+#endif 
---
+++ User/composition/bootloader_composition.h
@@ -0,0 +1,6 @@
+#ifndef __BOOTLOADER_COMPOSITION_H
+#define __BOOTLOADER_COMPOSITION_H
+
+void bootloader_composition_init(void);
+
+#endif 
